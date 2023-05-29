c1f0e1156ed3d884423b725e491269c426471a82
PrototypeTrousers <salomaoo@gmail.com>
2023-05-28 22:38:45 -0300

    moves and centers

--- src/main/java/mechanicalarms/common/block/BlockBelt.java
+++ src/main/java/mechanicalarms/common/block/BlockBelt.java
@@ -4,6 +4,8 @@ import mechanicalarms.common.tile.BeltEntity;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
@@ -12,9 +14,13 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class BlockBelt extends HorizontalFacingBlock implements BlockEntityProvider {
+
+    BeltEntity be;
+
     public BlockBelt() {
         super(FabricBlockSettings.of(Material.METAL).strength(4.0f));
         setDefaultState(getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
@@ -28,7 +34,8 @@ public class BlockBelt extends HorizontalFacingBlock implements BlockEntityProvi
 
     @Override
     public @Nullable BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return new BeltEntity(pos, state);
+        be = new BeltEntity(pos, state);
+        return be;
     }
 
     @Override
@@ -41,8 +48,14 @@ public class BlockBelt extends HorizontalFacingBlock implements BlockEntityProvi
         return VoxelShapes.cuboid(0.0f, 0.0f, 0.0f, 1f, 3/16f, 1.0f);
     }
 
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        return ((world1, pos, state1, blockEntity) -> BeltEntity.tick(world1, pos, state1, (BeltEntity) blockEntity));
+    }
+
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return super.getPlacementState(ctx).with(Properties.HORIZONTAL_FACING, ctx.getPlayerFacing().getOpposite());
+        return super.getPlacementState(ctx).with(Properties.HORIZONTAL_FACING, ctx.getPlayerFacing());
     }
 }
--- src/main/java/mechanicalarms/common/tile/BeltEntity.java
+++ src/main/java/mechanicalarms/common/tile/BeltEntity.java
@@ -3,20 +3,59 @@ package mechanicalarms.common.tile;
 import mechanicalarms.MechanicalArmsMod;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.MovementType;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class BeltEntity extends BlockEntity {
     public BeltEntity(BlockPos pos, BlockState state) {
         super(MechanicalArmsMod.BELT_ENTITY, pos, state);
     }
 
-    public static void tick(World world, BlockPos pos, BlockState state, BeltEntity be) {
-        be.update();
+    public void update() {
+        List<Entity> entities = this.getWorld().getNonSpectatingEntities(Entity.class, new Box(pos.getX(), pos.getY(), pos.getZ(), pos.getX() + 1, pos.getY() +1, pos.getZ() + 1));
+        for (Entity e : entities) {
+            if (e.isAlive()) {
+                double xCenter = pos.getX() + 0.5f;
+                double zCenter = pos.getZ() + 0.5f;
+
+                double diffx = 0;
+                double diffz = 0;
+
+                if (e.getX() - xCenter > 0.1/16f) {
+                    diffx = -1/16f;
+                } else {
+                    diffx = 1/16f;
+                }
+
+                if (e.getZ() - zCenter > 0.1/16f) {
+                    diffz = -1/16f;
+                } else {
+                    diffz = 1/16f;
+                }
+
+                switch (world.getBlockState(this.pos).get(Properties.HORIZONTAL_FACING)) {
+                    case NORTH -> e.move(MovementType.SELF, new Vec3d(diffx,0, - 0.025));
+                    case SOUTH -> e.move(MovementType.SELF, new Vec3d(diffx,0, + 0.025));
+                    case WEST -> e.move(MovementType.SELF, new Vec3d(+ 0.025, 0, diffz));
+                    case EAST -> e.move(MovementType.SELF, new Vec3d( - 0.025, 0, diffz));
+                }
+                e.setOnGround(true);
+            }
+        }
     }
 
-    void update() {
-
+    public static void tick(World world, BlockPos pos, BlockState state, BeltEntity blockEntity) {
+        if (world.isClient()) {
+            return;
+        }
+        blockEntity.update();
     }
 
 }
